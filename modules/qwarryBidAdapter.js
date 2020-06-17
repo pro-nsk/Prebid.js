2d93deef4cabaa20f57c1bba9afbad3510df04b4
Alexander Kascheev <akascheev@asteriosoft.com>
2020-06-17 17:25:21 +0700

    remove bidId

--- modules/qwarryBidAdapter.js
+++ modules/qwarryBidAdapter.js
@@ -1,4 +1,5 @@
 import { registerBidder } from 'src/adapters/bidderFactory';
+import { deepClone } from '../src/utils';
 
 const BIDDER_CODE = 'qwarry';
 const ENDPOINT = 'https://ui-bidder.kantics.co/bid/adtag?prebid=true&zoneToken='
@@ -7,38 +8,42 @@ export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: ['banner', 'video'],
   aliases: ['ex'], // short code
-  
+
   isBidRequestValid: (bid) => {
     return bid.params && bid.params.zoneToken;
   },
+
   buildRequests: (validBidRequests, bidderRequest) => {
     let bid = validBidRequests[0];
     let zoneToken = bid.params.zoneToken;
     return {
       method: 'POST',
       url: ENDPOINT + zoneToken,
-      data: {},
-      bidId: bid.bidId
+      data: {}
     };
   },
+
   interpretResponse: (serverResponse, request) => {
     const serverBody = serverResponse.body;
 
-    let bid = Object.create(serverBody);
+    let bid = deepClone(serverBody);
     bid.cpm = parseFloat(serverBody.price);
-    bid.requestId = request.bidId;
 
     return [bid];
   },
+
   getUserSyncs: (syncOptions, serverResponses, gdprConsent, uspConsent) => {
 
   },
+
   onTimeout: (timeoutData) => {
 
   },
+
   onBidWon: (bid) => {
 
   },
+  
   onSetTargeting: (bid) => {
 
   }
