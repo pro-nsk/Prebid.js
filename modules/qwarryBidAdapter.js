308dff942cd19d70f39b9b9ab0f9552afaea269c
Alexander Kascheev <akascheev@asteriosoft.com>
2020-06-18 13:29:25 +0700

    win function

--- modules/qwarryBidAdapter.js
+++ modules/qwarryBidAdapter.js
@@ -1,5 +1,6 @@
 import { registerBidder } from 'src/adapters/bidderFactory';
 import { deepClone } from '../src/utils';
+import { ajax } from '../src/ajax';
 
 const BIDDER_CODE = 'qwarry';
 const ENDPOINT = 'https://ui-bidder.kantics.co/bid/adtag?prebid=true&zoneToken='
@@ -8,11 +9,11 @@ export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: ['banner', 'video'],
 
-  isBidRequestValid: (bid) => {
+  isBidRequestValid: function (bid) {
     return bid.params && bid.params.zoneToken;
   },
 
-  buildRequests: (validBidRequests, bidderRequest) => {
+  buildRequests: function (validBidRequests, bidderRequest) {
     let bid = validBidRequests[0];
     let zoneToken = bid.params.zoneToken;
     return {
@@ -22,14 +23,22 @@ export const spec = {
     };
   },
 
-  interpretResponse: (serverResponse, request) => {
+  interpretResponse: function (serverResponse, request) {
     const serverBody = serverResponse.body;
 
     let bid = deepClone(serverBody);
     bid.cpm = parseFloat(serverBody.price);
 
     return [bid];
-  }
+  },
+
+  onBidWon: function (bid) {
+    if (bid['winUrl']) {
+      ajax(bid['winUrl'], null);
+      return true;
+    }
+    return false;
+  },
 }
 
 registerBidder(spec);
